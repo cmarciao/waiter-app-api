707e7c16c33ad399ae2d9c0e7e9e99fbab6c7cb9
Cássio Marcião <marciaocassio@gmail.com>
2023-10-14 11:00:48 -0300

    style: Config eslint, editorconfig and prettier rules

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,20 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+    let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [AppController],
+            providers: [AppService],
+        }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello Waiter App!"', () => {
-      expect(appController.getHello()).toBe('Hello Waiter App!');
+        appController = app.get<AppController>(AppController);
     });
-  });
+
+    describe('root', () => {
+        it('should return "Hello Waiter App!"', () => {
+            expect(appController.getHello()).toBe('Hello Waiter App!');
+        });
+    });
 });
