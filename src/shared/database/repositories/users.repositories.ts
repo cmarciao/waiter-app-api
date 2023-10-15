c1c3be3ada5980377bc5532addc4eb2d65f61ab1
Cássio Marcião <marciaocassio@gmail.com>
2023-10-14 22:37:16 -0300

    feat: Add database module

---
+++ src/shared/database/repositories/users.repositories.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class UsersRepository {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    create(createUserDto: Prisma.UserCreateArgs) {
+        return this.prismaService.user.create(createUserDto);
+    }
+
+    findById(id: string) {
+        return this.prismaService.user.findUnique({
+            where: {
+                id,
+            },
+        });
+    }
+
+    findByEmail(email: string) {
+        return this.prismaService.user.findUnique({
+            where: {
+                email,
+            },
+        });
+    }
+}
